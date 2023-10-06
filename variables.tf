63a09d0ddbd1406ca420feafd7c54de0329029e5
Sadri Gargouri <sadri.gargouri@teliacompany.com>
2023-10-06 16:05:28 +0200

    add multi cluster support

--- variables.tf
+++ variables.tf
@@ -1,25 +1,44 @@
+# Variables
+
+# Set-like list of unique cluster names to determine the logging targets.
+variable "cluster_names" {
+  description = "Set-like list of unique cluster names."
+  type        = list(string)
+}
+
+# A flag to control if logs from multiple clusters should be aggregated into a single sink.
+variable "multiple_clusters_per_sink" {
+  description = "Boolean to determine if a single sink should handle logs for multiple clusters."
+  type        = bool
+  default     = false # Default is one cluster per sink
+}
+
+# Other essential variables
 variable "source_project" {
-  description = "The source project name."
+  description = "The source project ID from where the logs are fetched."
   type        = string
 }
 
 variable "destination_project" {
-  description = "The destination project name where logs will be sent."
+  description = "The destination project ID to where the logs will be stored."
   type        = string
 }
 
-variable "cluster_name" {
-  description = "The cluster name."
+# Optional custom log filters if the default isn't suitable.
+variable "custom_log_filter" {
+  description = "Optional custom filter string for logs that will apply to all clusters."
   type        = string
+  default     = ""
 }
 
 variable "bucket_location" {
-  description = "The location for the destination bucket."
+  description = "The location of the logging bucket."
   type        = string
   default     = "europe-north1"
 }
-variable "log_filter_custom" {
-  description = "Optional: Specify a custom filter if needed. If not provided, a default filter based on source_project and cluster_name will be used."
-  type        = string
-  default     = ""
+
+variable "retention_days" {
+  description = "Number of days to retain log entries."
+  type        = number
+  default     = 90
 }
